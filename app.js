9f467afee45602ba0d34ca014a205f239b570aa4
akibkhan1 <akibkhanrt@gmail.com>
2020-06-24 15:24:03 +0600

    fix behav.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let status=["","","","","",""];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use("/public", express.static('./public/'));
 
 app.get("/", function(req, res){
   res.render("index",{status_home: "active",
